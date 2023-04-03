23eaa4067945384e4f283b18ae13f7bbb4d4567d
ghulammeeran <ghulammeeranmirza222@gmail.com>
2023-04-02 22:32:22 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+var userNumber=+ prompt("enter a number")
+
+if( userNumber%5==0 && userNumber%11==0){
+    console.log("userNumber is divisible");
+}
+else{
+    console.log("userNumber is not divivible");
+}
